f71f7dec8bfc7d7b7d3f6d57ae8b54acdabe1796
SaschaZeGerman <sascha.preibisch@gmail.com>
2022-12-04 22:52:50 -0800

    feature/mvn, adding tests

--- net.loginbuddy.tools.client-sdk/src/main/java/net/loginbuddy/tools/client/SidecarClient.java
+++ net.loginbuddy.tools.client-sdk/src/main/java/net/loginbuddy/tools/client/SidecarClient.java
@@ -2,16 +2,21 @@ package net.loginbuddy.tools.client;
 
 import net.loginbuddy.tools.common.exception.LoginbuddyToolsException;
 import net.loginbuddy.tools.common.model.LoginbuddyResponse;
+import net.loginbuddy.tools.common.oidc.Prompt;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class SidecarClient {
@@ -23,20 +28,24 @@ public class SidecarClient {
     private String loginbuddyCallbackUrl;
 
     private SidecarClient() {
-        String loginbuddylocation = System.getenv("LOGINBUDDY_SIDECAR_LOCATION");
-        if(loginbuddylocation != null) {
+        try {
+            String loginbuddylocation = System.getenv("LOGINBUDDY_SIDECAR_LOCATION");
+            if (loginbuddylocation == null) {
+                LOGGER.info("Environment variable LOGINBUDDY_SIDECAR_LOCATION was not set, using default location: http://loginbuddy-sidecar:8044");
+                loginbuddylocation = "http://loginbuddy-sidecar:8044";
+            }
             loginbuddyInitUrl = loginbuddylocation.concat("/sidecar/initialize");
             loginbuddyCallbackUrl = loginbuddylocation.concat("/sidecar/callback?%s");
             LOGGER.info(String.format("ClientSDK connects to this location: %s", loginbuddylocation));
-        } else {
-            LOGGER.severe("ClientSDK cannot be initialized, missing environment variable LOGINBUDDY_SIDECAR_LOCATION");
+        } catch(RuntimeException e) {
+            LOGGER.severe(String.format("Environment variable LOGINBUDDY_SIDECAR_LOCATION is not accessible, clientSDK is not usable: %s", e.getMessage()));
         }
     }
 
     /**
      * This method initiates the process of creating the authorization URL that may be used to redirect a user to its authorization endpoint.
      *
-     * @param provider The provider for which to create the authorization Url. This value must match a 'provider' in Loginbuddys configuration!
+     * @param provider The provider for which to create the authorization Url. This value must match a 'provider' in Loginbuddy's config.json and must not be null!
      * @return
      */
     public static SidecarClientAuthRequest createAuthRequest(String provider) {
@@ -97,4 +106,155 @@ public class SidecarClient {
             throw new LoginbuddyToolsException(locationError);
         }
     }
+
+}
+class SidecarClientAuthRequest {
+
+    private final SidecarClient client;
+
+    private final List<NameValuePair> formParameters;
+
+    /**
+     * Create a new AuthorizationRequest
+     *
+     * @param client   Must not be null
+     * @param provider Must not be null
+     */
+    SidecarClientAuthRequest(SidecarClient client, String provider) {
+        this.client = client;
+        formParameters = new ArrayList<>();
+        formParameters.add(new BasicNameValuePair("provider", provider));
+    }
+
+    /**
+     * Call this method once all parameters are set.
+     *
+     * @return
+     */
+    public SidecarClient build() {
+        return client;
+    }
+
+    public List<NameValuePair> getParameters() {
+        return formParameters;
+    }
+
+    /**
+     * Optional: this will be returned, as is, after Loginbuddy has received a token response from a provider. Use this to manage the clients session state.
+     *
+     * @param state
+     * @return
+     */
+    public SidecarClientAuthRequest setState(String state) {
+        formParameters.add(new BasicNameValuePair("state", state));
+        return this;
+    }
+
+    /**
+     * Optional: Set the SCOPE which should be included in the authorization request URL. Any given value has to match what has been configured in Loginbuddys configuration for the current provider.
+     * If it is not set the configured scope for the given provider will be used.
+     *
+     * @param scope
+     * @return
+     */
+    public SidecarClientAuthRequest setScope(String scope) {
+        formParameters.add(new BasicNameValuePair("scope", scope));
+        return this;
+    }
+
+    /**
+     * Optional: Set the nonce value for the authorization request. If not set Loginbuddy will create one.
+     *
+     * @param nonce
+     * @return
+     */
+    public SidecarClientAuthRequest setNonce(String nonce) {
+        formParameters.add(new BasicNameValuePair("nonce", nonce));
+        return this;
+    }
+
+    public SidecarClientAuthRequest setDynamicProvider(String issuer) {
+        return setDynamicProvider(issuer, null);
+    }
+
+    public SidecarClientAuthRequest setDynamicProvider(String issuer, String discoveryUrl) {
+        formParameters.add(new BasicNameValuePair("issuer", issuer));
+        if (discoveryUrl != null) {
+            formParameters.add(new BasicNameValuePair("discovery_url", discoveryUrl));
+        }
+        formParameters.add(new BasicNameValuePair("provider", "")); // needs to be empty to enable dynamic registration
+        formParameters.add(new BasicNameValuePair("accept_dynamic_provider", "true"));
+        return this;
+    }
+
+    /**
+     * Optional: This value will be forwarded to the given provider as is. This parameter is taken out of the OpenID Connect core specification.
+     * Loginbuddy will not include this parameter if it is not set.
+     *
+     * @param prompt
+     * @return
+     */
+    public SidecarClientAuthRequest setPrompt(Prompt prompt) {
+        formParameters.add(new BasicNameValuePair("prompt", prompt.toString()));
+        return this;
+    }
+
+
+    /**
+     * This value will be forwarded to the given provider as is. This parameter is taken out of the OpenID Connect core specification.
+     * Loginbuddy will not include this parameter if it is not set.
+     *
+     * @param loginHint
+     * @return
+     */
+    public SidecarClientAuthRequest setLoginHint(String loginHint) {
+        formParameters.add(new BasicNameValuePair("login_hint", loginHint));
+        return this;
+    }
+
+    /**
+     * This value will be forwarded to the given provider as is. This parameter is taken out of the OpenID Connect core specification.
+     * Loginbuddy will not include this parameter if it is not set.
+     *
+     * @param idTokenHint
+     * @return
+     */
+    public SidecarClientAuthRequest setIdTokenHint(String idTokenHint) {
+        formParameters.add(new BasicNameValuePair("id_token_hint", idTokenHint));
+        return this;
+    }
+
+    /**
+     * If set your client receives a JWT (signed by loginbuddy-sidecar) as response instead of a JSON document.
+     * The JWT will be signed using alg=RS256.
+     *
+     * @return
+     */
+    public SidecarClientAuthRequest setSignedResponseAlgRS256() {
+        formParameters.add(new BasicNameValuePair("signed_response_alg", "RS256"));
+        return this;
+    }
+
+    /**
+     * If set your client receives a JWT (signed by loginbuddy-sidecar) as response instead of a JSON document.
+     * The JWT will be signed using alg=ES256.
+     *
+     * @return
+     */
+    public SidecarClientAuthRequest setSignedResponseAlgES256() {
+        formParameters.add(new BasicNameValuePair("signed_response_alg", "ES256"));
+        return this;
+    }
+
+    /**
+     * If set the providers access_token and refresh_token will be obfuscated before returned to your application.
+     * If not set the original values will be passed back to your application.
+     *
+     * @return
+     */
+    public SidecarClientAuthRequest setObfuscateToken() {
+        formParameters.add(new BasicNameValuePair("obfuscate_token", "true"));
+        return this;
+    }
+
 }
--- net.loginbuddy.tools.client-sdk/src/test/java/net/loginbuddy/tools/client/SidecarClientAuthRequestTest.java
+++ net.loginbuddy.tools.client-sdk/src/test/java/net/loginbuddy/tools/client/SidecarClientAuthRequestTest.java
@@ -1,4 +1,57 @@
 package net.loginbuddy.tools.client;
 
+import net.loginbuddy.tools.common.exception.LoginbuddyToolsException;
+import net.loginbuddy.tools.common.oidc.Prompt;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SidecarClientAuthRequestTest {
+    private SidecarClientAuthRequest scar;
+
+    @Test
+    public void testAllParameters() {
+
+        scar = SidecarClient.createAuthRequest("providerName");
+
+        scar.setState("myState");
+        scar.setScope("myScope");
+        scar.setNonce("myNonce");
+        scar.setDynamicProvider("https://myIssuer:1234/value");
+        scar.setPrompt(Prompt.LOGIN_CONSENT);
+        scar.setLoginHint("myLogin@Hint.com");
+        scar.setIdTokenHint("myIdTokenHint");
+        scar.setSignedResponseAlgES256();  // RS256 is also available but the last one set wins
+        scar.setObfuscateToken();
+
+        assertEquals("providerName", scar.getParameters().stream().filter(param -> "provider".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("myScope", scar.getParameters().stream().filter(param -> "scope".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("myNonce", scar.getParameters().stream().filter(param -> "nonce".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("https://myIssuer:1234/value", scar.getParameters().stream().filter(param -> "issuer".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("login consent", scar.getParameters().stream().filter(param -> "prompt".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("myLogin@Hint.com", scar.getParameters().stream().filter(param -> "login_hint".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("myIdTokenHint", scar.getParameters().stream().filter(param -> "id_token_hint".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("ES256", scar.getParameters().stream().filter(param -> "signed_response_alg".equals(param.getName())).findFirst().orElse(null).getValue());
+        assertEquals("true", scar.getParameters().stream().filter(param -> "obfuscate_token".equals(param.getName())).findFirst().orElse(null).getValue());
+    }
+
+    @Test
+    public void testProviderNull() {
+        scar = SidecarClient.createAuthRequest(null);
+        assertNull(scar.getParameters().stream().filter(param -> "provider".equals(param.getName())).findFirst().orElse(null).getValue());
+    }
+
+    @Test
+    public void testNoConnection() {
+        scar = SidecarClient.createAuthRequest("myProvider");
+        try {
+            scar.build().getAuthorizationUrl();
+            fail("No connection to loginbudyd-sidecar, should fail");
+        } catch (LoginbuddyToolsException e) {
+            assertEquals("error_description", e.getErrorDescription());
+            assertEquals("error", e.getError());
+            assertTrue(e.getMessage(), true);
+        }
+    }
 }
